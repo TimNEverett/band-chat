bcba68b9658c4534606a677a14354a466ad31e4e
Tim Everett <tim.neil.everett@gmail.com>
2023-01-09 21:24:55 -0400

    fix: foxus chat input after sending message

--- src/components/Chat/ChatInput.tsx
+++ src/components/Chat/ChatInput.tsx
@@ -1,7 +1,7 @@
 // Make a chat input component
 // This component will be used to send messages to the chat
 
-import React, { useState } from 'react'
+import React, { useState, useRef } from 'react'
 import { useChatContext } from '../../contexts/chat.context'
 import Input from '../common/Input'
 import SendIcon from '../common/Icons/SendIcon'
@@ -9,12 +9,14 @@ import SendIcon from '../common/Icons/SendIcon'
 export const ChatInput = () => {
   const { sendMessage } = useChatContext()
   const [message, setMessage] = useState('')
+  const inputRef = useRef<HTMLInputElement>(null)
 
   const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault()
     if (message && message.trim()) {
       await sendMessage(message)
       setMessage('')
+      inputRef.current?.focus()
     }
   }
 
@@ -24,6 +26,7 @@ export const ChatInput = () => {
         className="h-10 flex-1 rounded-l-lg rounded-r-none border border-black bg-white"
         value={message}
         onChange={(e) => setMessage(e.target.value)}
+        ref={inputRef}
       />
       <button type="submit" className="rounded-r-lg bg-black text-white px-4 h-10">
         <SendIcon />
--- src/components/common/Input.tsx
+++ src/components/common/Input.tsx
@@ -1,7 +1,7 @@
 // react input component
 // use typescript and tailwindcss
 // props: value, onChange, placeholder, type, className, disabled
-import { FC, InputHTMLAttributes } from 'react'
+import React, { FC, InputHTMLAttributes } from 'react'
 
 type InputProps = InputHTMLAttributes<HTMLInputElement> & {
   value: string
@@ -12,17 +12,22 @@ type InputProps = InputHTMLAttributes<HTMLInputElement> & {
   disabled?: boolean
 }
 
-const Input: FC<InputProps> = ({ value, onChange, placeholder, type = 'text', className = '', disabled = false }) => {
-  return (
-    <input
-      value={value}
-      onChange={onChange}
-      placeholder={placeholder}
-      type={type}
-      className={`h-10 px-4 border rounded-full focus:outline-none ${className}`}
-      disabled={disabled}
-    />
-  )
-}
+const Input = React.forwardRef<HTMLInputElement, InputProps>(
+  ({ value, onChange, placeholder, type = 'text', className = '', disabled = false }, forwardedRef) => {
+    return (
+      <input
+        value={value}
+        onChange={onChange}
+        placeholder={placeholder}
+        type={type}
+        className={`h-10 px-4 border rounded-full focus:outline-none ${className}`}
+        disabled={disabled}
+        ref={forwardedRef}
+      />
+    )
+  },
+)
+
+Input.displayName = 'Input'
 
 export default Input

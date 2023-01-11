ba1d53ed62024408c9404a7cdf42956367ec45b0
Tim Everett <tim.neil.everett@gmail.com>
2023-01-11 08:41:37 -0400

    feat: added pwa support

---
+++ next.config.js
@@ -0,0 +1,14 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const withPWA = require('next-pwa')({
+  dest: 'public',
+})
+
+module.exports = withPWA({
+  reactStrictMode: true,
+  pwa: {
+    dest: 'public',
+    register: true,
+    skipWaiting: true,
+    disable: process.env.NODE_ENV === 'development',
+  },
+})

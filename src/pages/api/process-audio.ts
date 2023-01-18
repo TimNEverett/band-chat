4cd3340521ca3f8642b2648ea3f104518ae7b06c
Tim Everett <tim.neil.everett@gmail.com>
2023-01-17 22:20:34 -0400

    fix: process-audio api uses buffers instead of writing to file system

--- src/pages/api/process-audio.ts
+++ src/pages/api/process-audio.ts
@@ -3,15 +3,11 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 import ffmpeg from 'fluent-ffmpeg'
 import ffmpegStatic from 'ffmpeg-static'
-import fs from 'fs/promises'
 import { createClient } from '@supabase/supabase-js'
-
-const tempFileName = 'temp.mp3'
 
 const ProcessAudio = async (req: NextApiRequest, res: NextApiResponse): Promise<void> => {
   const start = Date.now()
   try {
-    console.log('Request received', req.body)
     const { fileName, secret } = typeof req.body == 'string' ? JSON.parse(req.body) : req.body
 
     if (secret !== process.env.API_SECRET)
@@ -25,40 +21,37 @@ const ProcessAudio = async (req: NextApiRequest, res: NextApiResponse): Promise<
 
     const { data, error } = await supabase.storage.from('audio-blobs').createSignedUrl(fileName, 60 * 60 * 24)
 
-    if (error) throw new Error(`Get Audio blob error: Could not get signed url: ${error.message}`)
+    if (error) throw new Error(`Get Audio blob error: Could not get signed url: ${error}`)
 
     // check if there is a local file called blob-1
     if (!data?.signedUrl) throw new Error('No file found')
-
+    const outputData: any[] = []
     // wrap audio conversion in a promise
     const convertAudio = () =>
       new Promise<boolean>((resolve, reject) => {
-        const process = ffmpeg().format('mp3').input(data.signedUrl).format('mp3').output(tempFileName)
+        const process = ffmpeg().format('mp3').input(data.signedUrl).format('mp3').pipe()
+        process.on('data', (chunk) => {
+          outputData.push(chunk)
+        })
         process.on('end', () => {
-          console.log('conversion ended')
           resolve(true)
         })
         process.on('error', (error) => {
           reject(false)
-          throw new Error(`conversion error ${error}`)
+          throw new Error(`stream error ${error}`)
         })
-        process.run()
       })
 
-    //await audio conversion
     const conversionSuccess = await convertAudio()
+
+    const buffer = Buffer.concat(outputData)
 
     if (!conversionSuccess) throw new Error('Could not convert audio')
 
-    // read the team file and upload it to supabase
-    const file = await fs.readFile(tempFileName)
     const { error: uploadError } = await supabase.storage
       .from('mp3s')
-      .upload(`${fileName}.mp3`, file, { contentType: 'audio/mpeg3' })
+      .upload(`${fileName}.mp3`, buffer, { contentType: 'audio/mpeg3' })
     if (uploadError) throw new Error(`Could not upload file: ${uploadError.message}`)
-
-    //delete the temp file
-    await fs.rm(tempFileName)
 
     res.json({ data: { success: true } })
   } catch (error: unknown) {
